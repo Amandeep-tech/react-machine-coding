2d6b7db4d436df083e8997114413075edab3c5fe
Amandeep Singh <ps772117@gmail.com>
2026-01-09 13:44:43 +0530

    in modal focus

--- 07-modal/src/App.jsx
+++ 07-modal/src/App.jsx
@@ -11,6 +11,14 @@ export default function App() {
         <Modal onClose={() => setShowModal(false)}>
           <h2>Hello Modal</h2>
           <p>This is modal content</p>
+          <p>Few Focusable elements</p>
+          <button>Focus 1</button>
+          <input type="text" name="" id="" />
+          <select name="" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+          <textarea name="" id=""></textarea>
         </Modal>
       ) : (
         <button onClick={() => setShowModal(true)}>Open Modal</button>
--- 07-modal/src/components/Modal.jsx
+++ 07-modal/src/components/Modal.jsx
@@ -1,23 +1,75 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 const Modal = ({ onClose, children }) => {
+  const closeBtnRef = useRef(null);
+
+  const modalContaineRef = useRef(null);
+
+  const previouslyFocusedEle = useRef(null);
+
   useEffect(() => {
-    const handleKeyDown = (e) => {
-      const key = e.key;
-      if (key === "Escape") {
-        onClose();
-      }
-    };
 
-    document.addEventListener("keydown", handleKeyDown);
 
-    return () => document.removeEventListener("keydown", handleKeyDown);
+    // save previous focused element
+    previouslyFocusedEle.current = document.activeElement
+    console.log("previouslyFocusedEle", previouslyFocusedEle);
+
+
+    // for some reason  'previouslyFocusedEle' is not working as expected.
+    // I expect the 'open modal' btn to be as previouslyFocusedEle but closeBtnRef 
+    // is getting stored in previouslyFocusedEle :(
+
+    if (closeBtnRef.current) {
+      closeBtnRef.current?.focus();
+    }
+    return () => {
+      // focus the previously focused element
+      previouslyFocusedEle.current?.focus();
+    }
   }, []);
+
+  useEffect(() => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") {
+      onClose();
+      return;
+    }
+
+    if (e.key !== "Tab") return;
+
+    const focusableElements = modalContaineRef.current.querySelectorAll(
+      "button, input, select, textarea, [tabindex]:not([tabindex='-1'])"
+    );
+
+    if (!focusableElements.length) return;
+
+    const first = focusableElements[0];
+    const last = focusableElements[focusableElements.length - 1];
+
+    if (e.shiftKey) {
+      // Shift + Tab
+      if (document.activeElement === first) {
+        e.preventDefault();
+        last.focus();
+      }
+    } else {
+      // Tab
+      if (document.activeElement === last) {
+        e.preventDefault();
+        first.focus();
+      }
+    }
+  };
+
+  document.addEventListener("keydown", handleKeyDown);
+  return () => document.removeEventListener("keydown", handleKeyDown);
+}, [onClose]);
+
   return (
     <div className="modal">
       <div className="backdrop" onClick={onClose} />
-      <div className="modal_container">
-        <button className="close_btn" onClick={onClose}>
+      <div className="modal_container" ref={modalContaineRef}>
+        <button ref={closeBtnRef} className="close_btn" onClick={onClose}>
           X
         </button>
         {children}
