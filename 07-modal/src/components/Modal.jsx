ca62dec10a3c2bd72796b717081f1d811a97a3da
Amandeep Singh <ps772117@gmail.com>
2026-01-09 14:17:25 +0530

    portal for modal

--- 07-modal/src/components/Modal.jsx
+++ 07-modal/src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const Modal = ({ onClose, children }) => {
   const closeBtnRef = useRef(null);
@@ -8,15 +9,12 @@ const Modal = ({ onClose, children }) => {
   const previouslyFocusedEle = useRef(null);
 
   useEffect(() => {
-
-
     // save previous focused element
-    previouslyFocusedEle.current = document.activeElement
+    previouslyFocusedEle.current = document.activeElement;
     console.log("previouslyFocusedEle", previouslyFocusedEle);
 
-
     // for some reason  'previouslyFocusedEle' is not working as expected.
-    // I expect the 'open modal' btn to be as previouslyFocusedEle but closeBtnRef 
+    // I expect the 'open modal' btn to be as previouslyFocusedEle but closeBtnRef
     // is getting stored in previouslyFocusedEle :(
 
     if (closeBtnRef.current) {
@@ -25,47 +23,47 @@ const Modal = ({ onClose, children }) => {
     return () => {
       // focus the previously focused element
       previouslyFocusedEle.current?.focus();
-    }
+    };
   }, []);
 
   useEffect(() => {
-  const handleKeyDown = (e) => {
-    if (e.key === "Escape") {
-      onClose();
-      return;
-    }
-
-    if (e.key !== "Tab") return;
-
-    const focusableElements = modalContaineRef.current.querySelectorAll(
-      "button, input, select, textarea, [tabindex]:not([tabindex='-1'])"
-    );
-
-    if (!focusableElements.length) return;
-
-    const first = focusableElements[0];
-    const last = focusableElements[focusableElements.length - 1];
-
-    if (e.shiftKey) {
-      // Shift + Tab
-      if (document.activeElement === first) {
-        e.preventDefault();
-        last.focus();
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+        return;
       }
-    } else {
-      // Tab
-      if (document.activeElement === last) {
-        e.preventDefault();
-        first.focus();
+
+      if (e.key !== "Tab") return;
+
+      const focusableElements = modalContaineRef.current.querySelectorAll(
+        "button, input, select, textarea, [tabindex]:not([tabindex='-1'])"
+      );
+
+      if (!focusableElements.length) return;
+
+      const first = focusableElements[0];
+      const last = focusableElements[focusableElements.length - 1];
+
+      if (e.shiftKey) {
+        // Shift + Tab
+        if (document.activeElement === first) {
+          e.preventDefault();
+          last.focus();
+        }
+      } else {
+        // Tab
+        if (document.activeElement === last) {
+          e.preventDefault();
+          first.focus();
+        }
       }
-    }
-  };
+    };
 
-  document.addEventListener("keydown", handleKeyDown);
-  return () => document.removeEventListener("keydown", handleKeyDown);
-}, [onClose]);
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [onClose]);
 
-  return (
+  return createPortal(
     <div className="modal">
       <div className="backdrop" onClick={onClose} />
       <div className="modal_container" ref={modalContaineRef}>
@@ -74,7 +72,8 @@ const Modal = ({ onClose, children }) => {
         </button>
         {children}
       </div>
-    </div>
+    </div>,
+    document.getElementById("modal_root")
   );
 };
 
