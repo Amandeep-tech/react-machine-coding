e7bba8cd32e4f41047bab7898e415decbbc765fe
Amandeep Singh <ps772117@gmail.com>
2026-01-12 17:56:16 +0530

    auto close after 3seconds

--- 10-toast/src/components/Toast.jsx
+++ 10-toast/src/components/Toast.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useEffect } from "react";
 
 const Toast = ({ toast, removeToast }) => {
   const { message } = toast;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(toast.id);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [toast.id, removeToast]);
   return (
     <div className="toast">
       <div>{message}</div>
-      <button onClick={() => removeToast(toast.id)} className="cross">x</button>
+      <button onClick={() => removeToast(toast.id)} className="cross">
+        x
+      </button>
     </div>
   );
 };
