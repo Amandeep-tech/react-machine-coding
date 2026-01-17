b485574fd12800b45d347af9b5ccaff57e671c93
Amandeep Singh <ps772117@gmail.com>
2026-01-17 13:53:38 +0530

    implement infinite scroll with data fetching and loading states

--- 11-infinite-scroll/src/components/InfiniteScroll.jsx
+++ 11-infinite-scroll/src/components/InfiniteScroll.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const fetchItems = (page) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      const items = Array.from({ length: 10 }, (_, i) => ({
+        id: page * 10 + i,
+        text: `Item ${page * 10 + i}`,
+      }));
+      resolve(items);
+    }, 1000);
+  });
 
 const InfiniteScroll = () => {
-  return (
-    <div>InfiniteScroll</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-export default InfiniteScroll
+  const [hasMoreData, setHasMoreData] = useState(true);
+
+  useEffect(() => {
+    if(!hasMoreData) return;
+
+    async function fetchData(page) {
+      try {
+        setLoading(true);
+        setError("");
+        const resp = await fetchItems(page);
+
+        if(resp && resp.length === 0) {
+          setHasMoreData(false);
+          return;
+        }
+
+        if (resp && resp.length > 0) {
+          // immutable updates
+          setData((prev) => [...prev, ...resp]);
+        }
+        
+      } catch (err) {
+        console.error("error ", err);
+        setError("Error Occured");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData(page);
+  }, [page]);
+
+  return (
+    <div className="infinite_scroll">
+      {error ? <div>{error}</div> : null}
+      {loading ? <div>Loading...</div> : null}
+      <ul>
+        {data && data.length > 0 ? (
+          <>
+            {data.map((item) => (
+              <li key={item.id}>{item.text}</li>
+            ))}
+          </>
+        ) : null}
+      </ul>
+      {!loading && (!data || data.length === 0) ? (
+        <div>No Data, try again</div>
+      ) : null}
+    </div>
+  );
+};
+
+export default InfiniteScroll;
