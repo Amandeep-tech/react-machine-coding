96c502ca123560903498afac913356fa1cbf4318
Amandeep Singh <ps772117@gmail.com>
2026-01-28 18:53:28 +0530

    reuseable infinite scroll component

--- 11-infinite-scroll/src/App.jsx
+++ 11-infinite-scroll/src/App.jsx
@@ -1,10 +1,68 @@
+import { useEffect, useState } from "react";
 import InfiniteScroll from "./components/InfiniteScroll";
 import "./styles.css";
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const [hasMoreData, setHasMoreData] = useState(true);
+
+  useEffect(() => {
+    fetchData(pageNumber)
+  }, [pageNumber])
+
+  const fetchData = async (page) => {
+    try {
+      if (!hasMoreData || loading) return;
+      setLoading(true);
+      setError(false);
+      let resp = await fetch(
+        `https://openlibrary.org/search.json?q=page=${page}&limit=${100}`,
+      );
+      if (!resp.ok) {
+        throw new Error("Response is not ok");
+      }
+      resp = await resp.json();
+      setData((prev) => [...prev, ...resp.docs]);
+      setHasMoreData(resp.docs.length > 0);
+      console.log("resp ", resp);
+    } catch (err) {
+      console.err("Error", err);
+      setError("Something went wrong, try again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loadData = () => {
+    setPageNumber(prev => prev + 1);
+  }
+
   return (
     <div className="app">
-      <InfiniteScroll />
+      <InfiniteScroll
+        data={data}
+        loadData={loadData}
+        hasMoreData={hasMoreData}
+        loading={loading}
+        error={error}
+        renderItem={(item) => <ItemCard item={item} />}
+      />
     </div>
   );
 }
+
+const ItemCard = (props) => {
+  const { item } = props;
+  return <div 
+    style={{
+      border: "1px solid grey",
+      borderRadius: "8px",
+      padding: "10px"
+    }}
+  >{item.title}</div>;
+};
--- 11-infinite-scroll/src/components/InfiniteScroll.jsx
+++ 11-infinite-scroll/src/components/InfiniteScroll.jsx
@@ -4,21 +4,11 @@ import { useEffect, useRef, useState } from "react";
 // when last element comes in view port, loadMoreData()
 // repeat this process until API says hasMore is false or data length is 0
 
-export default function InfiniteScroll() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const [hasMoreData, setHasMoreData] = useState(true);
+export default function InfiniteScroll(props) {
+  const { data, loadData, hasMoreData, loading, error, renderItem } = props;
 
   const observerRef = useRef(null);
   const lastElementRef = useRef(null);
-
-  useEffect(() => {
-    void fetchData(pageNumber);
-  }, [pageNumber]);
 
   useEffect(() => {
     if (loading) return;
@@ -29,7 +19,7 @@ export default function InfiniteScroll() {
 
     observerRef.current = new IntersectionObserver((entries) => {
       if (entries[0].isIntersecting && hasMoreData) {
-        setPageNumber((prev) => prev + 1);
+        loadData()
       }
     });
 
@@ -40,47 +30,24 @@ export default function InfiniteScroll() {
     return () => observerRef.current?.disconnect();
   }, [loading, hasMoreData]);
 
-  const fetchData = async (page) => {
-    try {
-      if (!hasMoreData || loading) return;
-      setLoading(true);
-      setError(false);
-      let resp = await fetch(
-        `https://openlibrary.org/search.json?q=page=${page}&limit=${100}`,
-      );
-      if (!resp.ok) {
-        throw new Error("Response is not ok");
-      }
-      resp = await resp.json();
-      setData((prev) => [...prev, ...resp.docs]);
-      setHasMoreData(resp.docs.length > 0);
-      console.log("resp ", resp);
-    } catch (err) {
-      console.err("Error", err);
-      setError("Something went wrong, try again");
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <div className="infinite_scroll">
+      {error ? <div>{error}</div> : null}
       {data?.length > 0 ? (
         <ul>
           {data.map((item, index) => {
             if (index === data.length - 1) {
               return (
                 <li ref={lastElementRef} key={item.title}>
-                  {item.title}
+                  {renderItem(item)}
                 </li>
               );
             }
-            return <li key={item.title}>{item.title}</li>;
+            return <li key={item.title}>{renderItem(item)}</li>;
           })}
         </ul>
       ) : null}
       {loading ? <div>Loading data...</div> : null}
-      {error ? <div>{error}</div> : null}
     </div>
   );
 }
